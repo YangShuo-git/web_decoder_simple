282be6790c6707ff985f18365a8d265edfe76b9d
baiyang <17801003317@163.com>
2022-08-19 02:01:28 +0800

    可以解码播放

---
+++ 3rdlibs/ffmpeg-4.4.1/libavformat/demuxer_list.c
@@ -0,0 +1,3 @@
+static const AVInputFormat * const demuxer_list[] = {
+    &ff_h264_demuxer,
+    NULL };
---
+++ 3rdlibs/ffmpeg-4.4.1/libavformat/muxer_list.c
@@ -0,0 +1,3 @@
+static const AVOutputFormat * const muxer_list[] = {
+    &ff_h264_muxer,
+    NULL };
